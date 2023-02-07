1c5a6a0948dc651a6e65baf199e1a7170ac2e489
VictoriaColta <65483439+VictoriaColta@users.noreply.github.com>
2023-02-07 09:09:40 +0200

    Update DragAndDrop.java

--- src/test/java/testsPractice/DragAndDrop.java
+++ src/test/java/testsPractice/DragAndDrop.java
@@ -1,4 +1,12 @@
 package testsPractice;
 
 public class DragAndDrop {
+   //Initializam clasa Actions
+        Actions actions = new Actions(driver());
+
+        //Indentificam elementele cu care vom interactiona
+
+        //apelam metoda dragAndDrop
+
+
 }
